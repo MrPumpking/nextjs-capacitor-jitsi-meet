eae790a08f4fba4a15a35488679f0ecd35a7dcf7
Michał Bar <logout400@gmail.com>
2021-04-29 12:37:24 +0200

    feat: add capacitor-jitsi-meet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,30 @@
 import {NextPage} from "next";
 
+import {Capacitor, Plugins} from '@capacitor/core';
+import 'capacitor-jitsi-meet';
+import {useState} from "react";
+
 const Home: NextPage = () => {
+  const [joinStatus, setJoinStatus] = useState('Waiting')
+
+  const joinConference = async () => {
+    const { Jitsi } = Plugins;
+
+    const result: any = await Jitsi.joinConference({
+      roomName: 'CapacitorJitsiMeetV2',
+      url: 'https://meet.jit.si',
+      displayName: 'user1'
+    });
+
+    setJoinStatus(result.success)
+  }
+
   return (
-    <div>Switch to capacitor-v2 or capacitor-v3 branch</div>
+    <div style={{display: 'flex', flexDirection: 'column'}}>
+      <span>Capacitor v2</span>
+      <span><b>Current platform:</b> {Capacitor.getPlatform()}</span>
+      <button onClick={joinConference}>Join Conference</button>
+    </div>
   )
 }
 
